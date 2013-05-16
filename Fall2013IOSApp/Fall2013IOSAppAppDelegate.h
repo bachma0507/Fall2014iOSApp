3f71fbac40e132b02401d3f2fca181b65544297f
Barry <barry@Barrys-MacBook-Pro.local>
2013-05-16 12:30:08 -0400

    Initial Commit

---
+++ Fall2013IOSApp/Fall2013IOSAppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Fall2013IOSAppAppDelegate.h
+//  Fall2013IOSApp
+//
+//  Created by Barry on 5/16/13.
+//  Copyright (c) 2013 BICSI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Fall2013IOSAppAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fall2013IOSApp/Fall2013IOSAppViewController.m
@@ -0,0 +1,29 @@
+//
+//  Fall2013IOSAppViewController.m
+//  Fall2013IOSApp
+//
+//  Created by Barry on 5/16/13.
+//  Copyright (c) 2013 BICSI. All rights reserved.
+//
+
+#import "Fall2013IOSAppViewController.h"
+
+@interface Fall2013IOSAppViewController ()
+
+@end
+
+@implementation Fall2013IOSAppViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
