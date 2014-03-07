b8a2cd7b2ec0c8450fcb282ababe88ced9554164
Barry <bachma0507@gmail.com>
2014-03-07 10:50:14 -0500

    added email notification when pic is submitted.

--- Canada2014IOSApp/IpadUploadImageViewController.m
+++ Canada2014IOSApp/IpadUploadImageViewController.m
@@ -206,6 +206,43 @@
                 
                 if (succeeded){
                     //Go back to the wall
+                    
+                    //Begin Send email
+                    NSString *post =[[NSString alloc] initWithFormat:@"message=%@",self.commentTextField.text];
+                    NSLog(@"PostData: %@",post);
+                    
+                    NSURL *url=[NSURL URLWithString:@"https://speedyreference.com/newpicsubmitted.php"];
+                    
+                    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+                    
+                    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+                    
+                    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+                    [request setURL:url];
+                    [request setHTTPMethod:@"POST"];
+                    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+                    //[request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+                    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+                    [request setHTTPBody:postData];
+                    
+                    NSError *error = [[NSError alloc] init];
+                    NSHTTPURLResponse *response = nil;
+                    NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+                    
+                    NSLog(@"Response code: %ld", (long)[response statusCode]);
+                    
+                    if ([response statusCode] >= 200 && [response statusCode] < 300)
+                    {
+                        NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+                        NSLog(@"Response ==> %@", responseData);
+                    }
+                    
+
+                    
+                    
+                    //End send email
+                    
+                    
                     [self.navigationController popViewControllerAnimated:YES];
                 }
                 else{
