a64393796a8066859ecd23032a2b9180a31a0162
Barry <bachma0507@gmail.com>
2014-03-09 03:03:08 -0400

    added main hall floor plan to iPhone storyboard

---
+++ Canada2014IOSApp/FloorMapsViewController.h
@@ -0,0 +1,15 @@
+//
+//  FloorMapsViewController.h
+//  Canada2014IOSApp
+//
+//  Created by Barry on 3/9/14.
+//  Copyright (c) 2014 BICSI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FloorMapsViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView2;
+
+@end
